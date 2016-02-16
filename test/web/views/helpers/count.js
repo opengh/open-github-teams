607d969333db0797cb82e9e6d931688d07e8b2a9
Martin Heidegger <martin.heidegger@gmail.com>
2016-02-16 22:23:59 +0900

    Added tests for count

---
+++ test/web/views/helpers/count.js
@@ -0,0 +1,15 @@
+var count = require('../../../../web/views/helpers/count')
+console.log(count)
+var assert = require('assert')
+
+describe('counting of items in hbs arrays', function () {
+  it('should support counting of arrays', function () {
+    assert.equal(count(['a']), 1)
+  })
+  it('should support counting other things', function () {
+    assert.equal(count(null), '?')
+  })
+  it('should support counting other things', function () {
+    assert.equal(count(''), '?')
+  })
+})
